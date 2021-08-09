201b33935d54dc37f9f3956d6ddde7521ac7413c
daydaybigking <daydaybigking@sina.com>
2021-08-10 00:12:56 +0800

    測試 emit 驗證效果

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Button @submit="handleSubmit"></Button>
+    <div v-if="false">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  methods: {
+    handleSubmit(number) {
+      console.log(number)
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Button
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="handleClick">Button</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  emits: {
+    submit: (number) => number < 10
+  },
+  methods: {
+    handleClick() {
+      this.$emit('submit', 10)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
